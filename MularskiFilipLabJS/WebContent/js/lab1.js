0dcea620df7f5c1a2b5e3fd2ba4fd9f3018ccdec
Filip Mularski <filipelblag@gmail.com>
2019-10-08 20:40:21 +0200

    Zad 5 - Quiz

--- MularskiFilipLabJS/WebContent/js/lab1.js
+++ MularskiFilipLabJS/WebContent/js/lab1.js
@@ -15,3 +15,33 @@ function myData()
 
   alert(imie + ' ' + nazwisko + ' grupa: ' + grupa + ' ' + indeks + ' ' + email);
 }
+
+var points = 0;
+var questionsLeft = 4;
+
+function btnAnswer(point, qn)
+{
+  points += point;
+  questionsLeft--;
+
+  let btnClassName = 'pyt' + qn + 'btn';
+  let btns = document.getElementsByClassName(btnClassName);
+  for ( i = 0; i < btns.length; i++)
+    btns[i].style.visibility = 'hidden';
+
+  if (questionsLeft == 0)
+    alert('Zdobyte punkty: ' + points);
+}
+
+function restartQuiz()
+{
+  points = 0;
+  questionsLeft = 4;
+  for ( i = 0; i < 4; i++)
+  {
+    let btnClassName = 'pyt' + (i + 1) + 'btn';
+    let btns = document.getElementsByClassName(btnClassName);
+    for ( j = 0; j < btns.length; j++)
+      btns[j].style.visibility = 'visible';
+  }
+}
