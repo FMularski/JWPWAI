b3b41791dda92db1d0dd93945d2782a6b7118bc0
Filip Mularski <filipelblag@gmail.com>
2019-10-08 19:43:12 +0200

    Zad 3 + 4

--- MularskiFilipLabJS/WebContent/js/lab1.js
+++ MularskiFilipLabJS/WebContent/js/lab1.js
@@ -4,3 +4,14 @@ var seasons = ['spring', 'summer', 'fall', 'winter'];
 var prime_numbers = [2, 3, 5, 7, 11, 13];
 
 var letters = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+function myData()
+{
+  var imie = document.getElementById("imie").textContent;
+  var nazwisko = document.getElementById("nazwisko").textContent;
+  var grupa = document.getElementById("grupa").textContent;
+  var indeks = document.getElementById("indeks").textContent;
+  var email = document.getElementById("email").textContent;
+
+  alert(imie + ' ' + nazwisko + ' grupa: ' + grupa + ' ' + indeks + ' ' + email);
+}
