8a4e9a4e2f83b8be909f758cc6e1b7e1c50aca1c
Filip Mularski <filipelblag@gmail.com>
2019-10-22 10:45:13 +0200

    Zad 2.2

--- MularskiFilipLabJS/WebContent/js/lab3.js
+++ MularskiFilipLabJS/WebContent/js/lab3.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // zad 1
   $('span.pq').each(function() {
     var quote=$(this).clone();
     quote.removeClass('pq');
@@ -6,9 +7,17 @@ $(document).ready(function() {
     $(this).before(quote);
   }); // koniec funkcji each
 
+  // zad 2.1
   $("*").dblclick(function (){
     let msg = "Kliknieto " + $(this);
     alert(msg);
+  });
+
+  // zad 2.2
+
+  $("a").hover(function(){
+    let message = "<p>Wskazałeś odnośnik myszą!</p>";
+    $('.main').append(message);
   });
 
 
